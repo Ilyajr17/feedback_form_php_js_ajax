48442187068585d173f65783730e2ef85296e8c5
Ilya Zherebtsov <izherebcov@gmail.com>
2022-07-27 14:47:58 +0400

    Создание файла sendMale.js для отправки данных и поключение к форме html

---
+++ js/forMail.js
@@ -0,0 +1,12 @@
+$('#sendMail').on('click', function() {
+let email = $('#email').val().trim();
+let name = $('#name').val().trim();
+let phone = $('#phone').val().trim();
+let message = $('#message').val().trim();
+
+if(email === '') {
+    
+    return false;
+}
+
+});
